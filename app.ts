13d598c7163eac46bbdc24f8d527d0e10ee83900
Yang <jiaoyang@douban.com>
2022-08-08 08:34:38 +0000

    feat: router history mode support

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import Koa from 'koa'
 import staticHost from 'koa-static'
 import bodyParser from 'koa-bodyparser'
 import { route } from './src/routers'
+import fs from 'fs/promises'
 
 const app = new Koa()
 
@@ -9,6 +10,12 @@ app
 .use(staticHost('dist'))
 .use(bodyParser())
 .use(route.routes())
+.use(async ctx => {
+    if (!ctx.body) {
+        ctx.body = await fs.readFile('dist/index.html', { encoding: 'utf-8'})
+    }
+})
+
 
 app.listen(3000, () => {
     console.log('app run@http://localhost:3000')
