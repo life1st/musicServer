8463d064b4d8a7dd352cc747424aa73fbdef9137
Yang <jiaoyang@douban.com>
2022-08-10 09:25:36 +0000

    feat: update album fe

--- src/model/albumModel.ts
+++ src/model/albumModel.ts
@@ -1,11 +1,15 @@
 import path from 'path'
-import Nedb from 'nedb'
+import fs from 'fs/promises'
+import Nedb from 'nedb-promises'
 import { DB_DIR } from '../utils/path'
 import { getDir } from '../utils/file'
+import { libraryModel } from './libraryModel'
+import { Music } from '../types/Music'
+import { Album } from '../types/Album'
 
 class AlbumModel {
     private dbFile = path.resolve(DB_DIR, 'albumModel')
-    private db = new Nedb({ filename: this.dbFile, autoload: true })
+    private db = Nedb.create(this.dbFile)
 
     constructor() {
         getDir(DB_DIR)
@@ -17,4 +21,45 @@ class AlbumModel {
             return this.db.find({artist})
         }
     }
+
+    async createAlbumFromLibrary () {
+        const LIMIT = 100
+        let hasMore = true
+        let page = 0
+        const albumNames: string[] = []
+        while(hasMore) {
+            const list = await libraryModel.getMusicList(page, LIMIT)
+            if (list.length === 0) {
+                hasMore = false
+                break
+            }
+            list.forEach(music => {
+                if (!albumNames.includes(music.album)) {
+                    albumNames.push(music.album)
+                    this.updateAlbum(music)
+                }
+            })
+        }
+    }
+
+    async updateAlbum(music: Music) {
+        const { album, artist, coverUrl } = music
+        const existAlbum = await this.db.findOne({album, artist}) as Album
+        let hasUpdate = false
+        if (existAlbum) {
+            
+            const { coverId: existCoverId } = existAlbum
+            
+            hasUpdate = await this.db.update({album, artist}, music, {}) > 0
+        } else {
+            const albumInfo: Album = 
+            hasUpdate = await this.db.insert() !== null
+        }
+        return hasUpdate
+    }
+
+    
 }
+const albumModel = new AlbumModel()
+
+export { albumModel }
