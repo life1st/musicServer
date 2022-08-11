82e954835065dca98fef6916cad825a9c8f52e9a
Yang <jiaoyang@douban.com>
2022-08-11 03:38:11 +0000

    feat: add songitem & play state

--- src/static/Components/Songlist.tsx
+++ src/static/Components/Songlist.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react'
 import * as style from './Songlist.module.less'
+import { musicState } from '../model/music'
+import { useRecoilValue } from 'recoil'
 import { Music } from '../../types/Music'
 import { useThrottleFn } from 'ahooks'
-import icLoading from '../imgs/ic-loading.svg'
-import icEmpty from '../imgs/ic-empty.svg'
 const { useRef } = React
 
+interface ItemProps {
+  music: Music;
+  onClick: () => void;
+}
+const SongItem = (props: ItemProps) => {
+  const { music: curPlaying } = useRecoilValue(musicState)
+  const { music, onClick = () => {} } = props
+  const isPlaying = curPlaying?.id === music.id
+  return (
+    <li onClick={onClick} className={style.songItem}>
+      <div className={style.content}>
+        <p className={style.name}>{music.title}</p>
+        <p className={style.artist}>{music.artist || '未知歌手'}</p>
+      </div>
+      { isPlaying ? (
+        <img src={require('../imgs/ic-cd.svg')} className={style.icPlaying} />
+      ) : null }
+    </li>
+  )
+}
 interface ILibrary {
     onItemClick: (m: Music, n: number) => void;
     onReachEnd: () => void;
@@ -34,11 +54,11 @@ const Songlist = (props: ILibrary) => {
     return (
         <ul className={style.container} onScroll={handleScroll} ref={listRef}>
             { list.map((item, i) => (
-                <li key={item.id} onClick={() => {handleItemClick(item, i)}}>{item.title}</li>
+                <SongItem key={item.id} music={item} onClick={() => { handleItemClick(item, i) }} />
             )) }
             { hasMore 
-              ? <img src={icLoading} className={style.iconEnd} /> 
-              : (<div className={style.emptyTips}><img src={icEmpty} className={style.icEmpty} /><span className={style.tipsText}>没有了</span></div>)  }
+              ? <img src={require('../imgs/ic-loading.svg')} className={style.iconEnd} /> 
+              : (<div className={style.emptyTips}><img src={require('../imgs/ic-empty.svg')} className={style.icEmpty} /><span className={style.tipsText}>没有了</span></div>)  }
         </ul>
     )
 }
