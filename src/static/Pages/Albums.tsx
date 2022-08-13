4bef136d5c59bd4baf9d6a6a68fe289326bbd05a
jiaoyang <jiaoyang@douban.com>
2022-08-13 15:21:40 +0800

    feat: common cover comp

--- src/static/Pages/Albums.tsx
+++ src/static/Pages/Albums.tsx
@@ -6,38 +6,28 @@ import { getAlbums } from '../API'
 import { Album } from '../../types/Album'
 import { useLoadmore } from '../hooks/useLoadmore'
 import Scroller from '../Components/Scroller'
+import Cover from '../Components/Cover'
 
-const { useCallback, useState } = React
 const { origin } = window.location
-const defaultCoverImg = require('../imgs/ic-album-default.svg')
 const Album = (props: Album) => {
   const { name, albumId } = props
-  const [ coverUrl, setCoverUrl ] = useState(`${origin}/file/album_cover/${albumId}`)
-
-  const handleLoadError = () => {
-    console.log(coverUrl)
-    setCoverUrl(defaultCoverImg)
-  }
 
   return (
     <div className={style.albumContainer} title={name}>
-      <img src={coverUrl} onError={handleLoadError} className={style.albumCoverImg} />
+      <Cover
+        src={`${origin}/file/album_cover/${albumId}`} 
+        className={style.albumCoverImg}
+      />
       <p className={style.albumTitle}>{name}</p>
     </div>
   )
 }
 
 const Albums = () => {
-  const fetchData = useCallback((pageNum) => {
-    return getAlbums(pageNum)
-  }, [])
-  const {list, loadNextPage, hasMore, loading, curPage} = useLoadmore({fetchData, listState: albumState})
+  const { list, loadNextPage, hasMore, loading } = useLoadmore({fetchData: getAlbums, listState: albumState})
   const memScrollTop = useRecoilValue(albumScrollState)
   const setAlbumScroll = useSetRecoilState(albumScrollState)
   
-  const handleScroll = (scrollTop) => {
-    setAlbumScroll(scrollTop)
-  }
   const handleReachEnd = () => {
     if (!loading && hasMore) {
       loadNextPage()
@@ -45,10 +35,10 @@ const Albums = () => {
   }
   return (
     <Scroller
+      onScroll={setAlbumScroll}
+      onReachEnd={handleReachEnd}
       initScrollTop={memScrollTop}
       className={style.albumsContainer}
-      onScroll={handleScroll}
-      onReachEnd={handleReachEnd}
       hasMore={hasMore}
       showLoading={hasMore}
     >
