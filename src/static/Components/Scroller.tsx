95643a90341db574444c09f9c30f72ef4381c385
jiaoyang <jiaoyang@douban.com>
2022-08-13 14:05:28 +0800

    feat: common scroller & loadmore hook

---
+++ src/static/Components/Scroller.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import * as style from './Scroller.module.less'
+import { useThrottleFn } from 'ahooks'
+
+const { useRef } = React
+
+interface Props {
+  onReachEnd?: () => void;
+  onScroll?: (scrollTop: number) => void;
+  showLoading?: boolean;
+  hasMore?: boolean;
+  className?: string
+}
+const Scroller = (props: React.PropsWithChildren<Props>) => {
+  const {
+    onReachEnd = () => {},
+    onScroll = () => {},
+    showLoading,
+    hasMore,
+    className
+  } = props
+
+  const listRef = useRef()
+  const { run: handleScroll, cancel, flush} = useThrottleFn(() => {
+    if (listRef.current) {
+      const { clientHeight, scrollTop, scrollHeight } = listRef.current
+      console.log(clientHeight, scrollTop, scrollHeight)
+      onScroll(scrollTop)
+      if (clientHeight + scrollTop >= scrollHeight - 20) {
+        onReachEnd?.()
+      }
+    }
+  }, { wait: 300 })
+
+  const renderEnd = () => {
+    if (showLoading && hasMore) {
+      return <img src={require('../imgs/ic-loading.svg')} className={style.icLoading} />
+    }
+    if (!hasMore) {
+      return (
+        <div className={style.emptyTips}>
+          <img src={require('../imgs/ic-empty.svg')} className={style.icEmpty} />
+          <span className={style.tipsText}>没有了</span>
+        </div>
+      )
+    }
+    return null
+  }
+  return (
+    <ul ref={listRef} onScroll={handleScroll} className={className}>
+      {props.children}
+      <li className={style.endFix}>{ renderEnd() }</li>
+    </ul>
+  )
+}
+
+export default Scroller
