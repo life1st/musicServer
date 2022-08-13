796e51fc52739fb804c24b29a3194b32a4ef1bb3
jiaoyang <jiaoyang@douban.com>
2022-08-13 23:15:06 +0800

    feat: full player ui

--- src/static/index.tsx
+++ src/static/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, useMatch } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
 import { Player } from './Components/Player'
 import { Pages } from './router'
@@ -8,22 +8,32 @@ import Menubar from './Components/Menubar'
 import './global.less'
 import 'reset-css'
 
-const App = () => {
+const { useMemo, Fragment} = React
 
+const App = () => {
+    const matchPlayer = useMatch('playing')
+    const pageStyles = useMemo(() => {
+        if (matchPlayer) {
+            return { flex: '0 1 0' }
+        }
+        return {}
+    }, [matchPlayer])
     return (
-        <Router>
-            <div className='pages-content-container'>
+        <Fragment>
+            <div className='pages-content-container' style={pageStyles}>
                 <Pages />
             </div>
             <Player />
             <Menubar />
-        </Router>
+        </Fragment>
     )
 }
 
 const root = createRoot(document.querySelector('.root'))
 root.render(
     <RecoilRoot>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </RecoilRoot>
 )
