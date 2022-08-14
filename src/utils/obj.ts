50004d7c288294f6106fd0331a1426919e3964c2
jiaoyang <jiaoyang@douban.com>
2022-08-14 22:10:33 +0800

    fix: type

--- src/utils/obj.ts
+++ src/utils/obj.ts
@@ -1,11 +1,13 @@
-export const excludeProps = <T>(obj: T, excludes: string[]): T => Object.keys(obj).reduce((acc, k) => {
+export const excludeProps = <T extends {}>(obj: T, excludes: string[]): T => 
+Object.keys(obj).reduce((acc, k) => {
   if (!excludes.includes(k)) {
       acc[k] = obj[k]
   }
   return acc;
 }, {} as T)
 
-export const filterExistProps = <T>(obj: T): T => Object.keys(obj).reduce((acc, k) => {
+export const filterExistProps = <T extends {}>(obj: T): T => 
+Object.keys(obj).reduce((acc, k) => {
     if (obj[k]) {
         acc[k] = obj[k]
     }
