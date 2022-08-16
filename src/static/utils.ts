829cbc67ea33065484a409d643185a6696c562bf
Yang <jiaoyangdev@gmail.com>
2022-08-16 11:23:16 +0000

    fix: copy on safari

--- src/static/utils.ts
+++ src/static/utils.ts
@@ -1,13 +1,24 @@
 let hasPermission = false
+
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 const checkPermission = () => {
     return !hasPermission ? navigator.permissions.query({name: 'clipboard-write'}).then((r) => r.state === 'granted') : true
 }
 
 export const copyText = async (text) => {
-    const hasPermission = await checkPermission()
-    if (hasPermission) {
-        navigator.clipboard.writeText(text)
-        return true
+    if (isSafari) {
+        const temp = document.createElement("input")
+        document.body.appendChild(temp)
+        temp.value = text
+        temp.select()
+        document.execCommand("copy")
+        temp.parentNode?.removeChild(temp)
+    } else {
+        const hasPermission = await checkPermission()
+        if (hasPermission) {
+            navigator.clipboard.writeText(text)
+            return true
+        }
+        return false
     }
-    return false
 }
