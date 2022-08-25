d105f677209dc401e3681bb585de988b4b6dc8a5
Yang <jiaoyangdev@gmail.com>
2022-08-25 07:45:45 +0000

    feat: static gzip

--- app.ts
+++ app.ts
@@ -1,18 +1,27 @@
 import Koa from 'koa'
 import staticHost from 'koa-static'
 import bodyParser from 'koa-bodyparser'
+import compress from 'koa-compress'
 import { route } from './src/routers'
 import { STATIC_DIR } from './src/utils/path'
 import { staticHandler } from './src/routers/static'
 
+const flush = require('zlib').constants.Z_SYNC_FLUSH
 const app = new Koa()
 
 app
+.use(compress({
+    filter: mime => /text|javascript|json/i.test(mime),
+    threshold: 1000,
+    gzip: { flush },
+    deflate: { flush },
+    br: false
+}))
 .use(staticHandler)
 .use(staticHost(STATIC_DIR, {
-    maxAge: 1000 * 60 * 60
+    maxAge: 1000 * 60 * 60,
 }))
-.use(bodyParser())
+.use(bodyParser({}))
 .use(route.routes())
 
 app.listen(3000, () => {
